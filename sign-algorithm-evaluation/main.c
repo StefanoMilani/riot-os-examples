0a5d6eba29d38de0b5c6d55d9fbab347c6a1a933
StefanoMilani <milani@diag.uniroma1.it>
2023-04-28 09:55:58 +0200

    Minor Fixes
    Signed-Off-By: Stefano Milani <milani@diag.uniroma1.it>

--- sign-algorithm-evaluation/main.c
+++ sign-algorithm-evaluation/main.c
@@ -8,7 +8,7 @@
 
 #include "utils.h"
 
-#define TESTROUNDS 16
+#define TESTROUNDS 512
 #define TEST_MESSAGE "Not so long test message."
 #define TEST_MESSAGE_LEN strlen(TEST_MESSAGE)
 
@@ -16,7 +16,7 @@ int main(void) {
   int res;
   uint32_t ecc_elapsed_time, aes_elapsed_time;
 
-	uECC_Curve curve = uECC_secp256r1();
+  uECC_Curve curve = uECC_secp256r1();
 
   // Keys Sizes and Initialization
   int private_key_size = uECC_curve_private_key_size(curve);
@@ -29,28 +29,29 @@ int main(void) {
   uint8_t aes_key[AES128_CMAC_BLOCK_SIZE];
   random_bytes(aes_key, AES128_CMAC_BLOCK_SIZE);
 
-
   // TEST ECC SIGN ALGORITHM ELAPSED TIME
   printf("\n====== ECC SIGN ALGORITHM TEST START =====\n");
   printf("Number of rounds: %u.\n", TESTROUNDS);
 
-  ecc_elapsed_time = ecc_sign((uint8_t *)TEST_MESSAGE, (size_t) TEST_MESSAGE_LEN, private_key, &curve, (unsigned) TESTROUNDS);
-  
-  printf("Average Elapsed Time: %u microseconds", (unsigned int) ecc_elapsed_time);
+  ecc_elapsed_time = ecc_sign((uint8_t*)TEST_MESSAGE, (size_t)TEST_MESSAGE_LEN,
+                              private_key, &curve, (unsigned)TESTROUNDS);
+
+  printf("Average Elapsed Time: %u microseconds",
+         (unsigned int)ecc_elapsed_time);
   printf("\n====== ECC SIGN ALGORITHM TEST STOP  =====\n");
-  
-  
+
   // TEST AES SIGN ALGORITHM ELAPSED TIME
   printf("\n====== AES CMAC ALGORITHM TEST START =====\n");
   printf("Number of rounds: %u.\n", TESTROUNDS);
 
-  // uint32_t aes_cmac(const uint8_t *msg, size_t msg_size, uint8_t *key, unsigned testrounds);
-  aes_elapsed_time = aes_cmac((uint8_t *)TEST_MESSAGE, (size_t) TEST_MESSAGE_LEN, aes_key, (unsigned) TESTROUNDS);
-  // 
-  printf("Average Elapsed Time: %u microseconds", (unsigned int) aes_elapsed_time);
+  // uint32_t aes_cmac(const uint8_t *msg, size_t msg_size, uint8_t *key,
+  // unsigned testrounds);
+  aes_elapsed_time = aes_cmac((uint8_t*)TEST_MESSAGE, (size_t)TEST_MESSAGE_LEN,
+                              aes_key, (unsigned)TESTROUNDS);
+  //
+  printf("Average Elapsed Time: %u microseconds",
+         (unsigned int)aes_elapsed_time);
   printf("\n====== AES CMAC ALGORITHM TEST STOP  =====\n");
-    
-    
+
   return 0;
- 
 }
